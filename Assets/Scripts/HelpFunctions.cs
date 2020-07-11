b055b0741a8346407ce85c19354d4ebcdbab4d89
johniponken <johnipohni@gmail.com>
2020-07-11 16:43:09 +0300

    Testing

--- Assets/Scripts/HelpFunctions.cs
+++ Assets/Scripts/HelpFunctions.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 class HelpFunctions
 {
-
     public static void MoveTheThing(Rigidbody2D rb, float X, float Y, float speed) {
         Vector2 dir = new Vector2(X,Y);
         dir.Normalize();
