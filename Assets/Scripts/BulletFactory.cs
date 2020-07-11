9faf88ca5883fc3c354bcef5f317df45c62f5a26
johniponken <johnipohni@gmail.com>
2020-07-11 17:02:12 +0300

    BulletFactory in progress

---
+++ Assets/Scripts/BulletFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletFactory : MonoBehaviour
+{
+    private GameObject bullet = GameObject.Find("Bullet");
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Instantiate(bullet, transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Instantiate(bullet, transform);
+    }
+}
