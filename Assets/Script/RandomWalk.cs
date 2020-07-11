b09d0e0e3c2f5333fb12bc1e2e487433cee32f38
Jim Magnusson <jim.magnusson97@gmail.com>
2020-07-11 16:41:45 +0300

    Added a placeholder enemy

---
+++ Assets/Script/RandomWalk.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomWalk : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //rb.velocity = RandomVector2D(-1f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private Vector2 RandomVector2D(float min, float max)
+    {
+        float x = Random.Range(min, max);
+        float y = Random.Range(min, max);
+        return new Vector2(x, y);
+    }
+}
