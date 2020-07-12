7399bd71fc5c3354e66358fbec06c1fff622fc33
johniponken <johnipohni@gmail.com>
2020-07-12 15:10:43 +0300

    Added Bullet Mitosis

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,15 +6,27 @@ public class Bullet : MonoBehaviour
 {
     public float speed;
     private Rigidbody2D rb;
+    private PrefabFactory pf = null;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        HelpFunctions.MoveTheThing(rb, transform.up, speed);
+        pf = GetComponent(typeof(PrefabFactory)) as PrefabFactory;
     }
 
     // Update is called once per frame
     void Update()
     {
-        HelpFunctions.MoveTheThing(rb, transform.up, speed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(gameObject);
+            pf.SpawnPrefabAtLocation("Bullet", collision.gameObject.transform.position, (transform.rotation * Quaternion.Euler(0f, 0f, 90)));
+            pf.SpawnPrefabAtLocation("Bullet", collision.gameObject.transform.position, (transform.rotation * Quaternion.Euler(0f, 0f, -90)));
+        }
     }
 }
--- Assets/Scripts/PrefabFactory.cs
+++ Assets/Scripts/PrefabFactory.cs
@@ -4,22 +4,32 @@ using UnityEngine;
 
 public class PrefabFactory : MonoBehaviour
 {
-    void SpawnPrefabInFront(string prefabName)
+
+    public void SpawnPrefabInFront(string prefabName)
     {
         SpawnPrefabInFront(prefabName, 1f);
     }
 
-    void SpawnPrefabInFront(string prefabName, float spawnDistance = 1f)
+
+    public void SpawnPrefabInFront(string prefabName, float spawnDistance)
     {
         Instantiate(Resources.Load(prefabName), transform.position + (transform.up * spawnDistance), transform.rotation);
     }
 
-    void SpawnPrefabAtLocation(string prefabName, Vector3 spawnLocation)
+
+    public void SpawnPrefabInFront(string prefabName, float spawnDistance, Quaternion spawnRotation)
+    {
+        Instantiate(Resources.Load(prefabName), transform.position + (transform.up * spawnDistance), spawnRotation);
+    }
+
+
+    public void SpawnPrefabAtLocation(string prefabName, Vector3 spawnLocation)
     {
         SpawnPrefabAtLocation(prefabName, spawnLocation, new Quaternion(0,0,0,0));
     }
 
-    void SpawnPrefabAtLocation(string prefabName, Vector3 spawnLocation, Quaternion spawnRotation = default(Quaternion))
+
+    public void SpawnPrefabAtLocation(string prefabName, Vector3 spawnLocation, Quaternion spawnRotation)
     {
         Instantiate(Resources.Load(prefabName), spawnLocation, spawnRotation);
     }
